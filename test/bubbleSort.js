ee0ff1131c833f41f4e4438d17e425420c0bcdb1
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-26 02:11:59 +0100

    added test comment

--- test/bubbleSort.js
+++ test/bubbleSort.js
@@ -1,5 +1,7 @@
 const { Bubble } = require("../index");
 
+// will be writing some proper tests soonest.
+
 let sortify = new Bubble();
 let array = [5, 56, 46, 3, 47, 4, 6];
 console.log(sortify.sort(array));
